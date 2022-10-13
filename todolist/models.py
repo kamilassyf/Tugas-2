db3309b196d7c7c6ea9b87f13f8d39d13e353e61
Syifa Cahya <syfcahyakamila@gmail.com>
2022-10-13 11:08:05 +0700

    tugas 6

--- todolist/models.py
+++ todolist/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add = True)
+    title = models.TextField()
     description = models.TextField()
     is_finished = models.BooleanField(default=False,null=True)
